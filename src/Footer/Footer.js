a97ef7c47be6fad61fa20c0827918917a711d52c
naraharikrishna007@gmail.com <naraharikrishna007@gmail.com>
2023-12-10 16:27:39 -0500

    project

---
+++ src/Footer/Footer.js
@@ -0,0 +1,17 @@
+
+
+
+function Footer() {
+    return (
+        <footer className="bottom">
+    
+        <div className="center">
+            All rights reserved &copy; Krishna Teja Naraharisetty
+        </div>
+    
+</footer>
+  
+    );
+  }
+  
+  export default Footer;
