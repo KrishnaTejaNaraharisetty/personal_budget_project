29a8d1bc35313237a461365c8ff07b91ee018d3e
naraharikrishna007@gmail.com <naraharikrishna007@gmail.com>
2023-12-11 09:00:20 -0500

    added server and client into one repo

--- cypress/e2e/AboutPage.cy.js
+++
@@ -1,8 +0,0 @@
-describe('About Page', () => {
-    it('should display the content of the About page', () => {
-      cy.visit('../src/AboutPage/AboutPage.js'); 
-      cy.get('.a').should('exist'); 
-      cy.contains('Personal budgeting is a fundamental financial practice').should('exist');
-    });
-  });
-  
